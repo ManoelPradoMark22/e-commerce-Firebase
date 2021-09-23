6e2fbca65473e8ee93badcded0097ef99efe1065
Manoel Prado <manoelprado.aecjr@gmail.com>
2021-09-23 13:58:49 -0300

    initialize fb definition

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 import lanchesArray from './lanches.js'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getDatabase, ref, onValue} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyBjp1xZR6T0fBs4uYST8PNV7rC2rUxNjpg",
+  authDomain: "e-commerce-3c41b.firebaseapp.com",
+  databaseURL: "https://e-commerce-3c41b-default-rtdb.firebaseio.com",
+  projectId: "e-commerce-3c41b",
+  storageBucket: "e-commerce-3c41b.appspot.com",
+  messagingSenderId: "352196438207",
+  appId: "1:352196438207:web:40360967b69b7bf4093e0a",
+  measurementId: "G-86PHKPHGWE"
+});
+
+const db = getDatabase(firebaseApp);
+const starCountRef = ref(db, 'sections/');
+
+/*onValue(starCountRef, (snapshot) => {
+  const data = snapshot.val();
+}); */
+
+let fbArray = [];
+
+function showArr(arr) {
+ fbArray = arr;
+}
+
+async function loadFirebase() {
+  await onValue(starCountRef, (snapshot) => {
+    showArr(snapshot.val());
+  });
+}
+
+await loadFirebase();
+
 //begin code
 
 let productsInCart = JSON.parse(sessionStorage.getItem('shoppingCart'));
@@ -8,7 +43,7 @@ if(!productsInCart){
 
 const arrLanches = lanchesArray();
 
-console.log(arrLanches);
+console.log(fbArray);
 
 const parentElement = document.querySelector('#buyItems');
 
